180ef636d1f92607a0ce4ff15bfe01e59211973e
bossm0n5t3r <bossm0n5t3r@gmail.com>
2020-12-02 04:37:47 +0900

    Add item03

---
+++ demo-effective-java/src/main/java/com/example/demoeffectivejava/item03/StaticFactory/Elvis.java
@@ -0,0 +1,17 @@
+package com.example.demoeffectivejava.item03.StaticFactory;
+
+public class Elvis {
+    private static final Elvis INSTANCE = new Elvis();
+    private Elvis() { }
+    public static Elvis getInstance() { return INSTANCE; }
+
+    public void leaveTheBuilding() {
+        System.out.println("Whoa baby, I'm outta here!");
+    }
+
+    // 이 메서드는 보통 클래스 바깥(다른 클래스)에 작성해야 한다!
+    public static void main(String[] args) {
+        Elvis elvis = Elvis.getInstance();
+        elvis.leaveTheBuilding();
+    }
+}
