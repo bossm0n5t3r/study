ca9cdb4dec6e071355e0147552e3f16dc3342e46
bossm0n5t3r <bossm0n5t3r@gmail.com>
2021-01-24 22:13:10 +0900

    Request object

--- demo-nestjs/src/Cats/app.controller.ts
+++ demo-nestjs/src/Cats/app.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Req } from '@nestjs/common';
 import { AppService } from '../app.service';
+import { Request } from 'express';
 
 @Controller('Cats')
 export class CatsController {
@@ -8,5 +9,10 @@ export class CatsController {
   @Get()
   getMeow(): string {
     return this.appService.getMeow();
+  }
+
+  @Get('RequestObject')
+  findAll(@Req() request: Request): string {
+    return 'This action returns all cats';
   }
 }
