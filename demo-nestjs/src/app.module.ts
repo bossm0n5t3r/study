e38a31c4762fad451f7cb91355228a0f1ef2ee03
bossm0n5t3r <bossm0n5t3r@gmail.com>
2021-01-30 12:23:31 +0900

    Rename app.controller.ts to cat.controller.ts

--- demo-nestjs/src/app.module.ts
+++ demo-nestjs/src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { CatsController } from './Cats/app.controller';
+import { CatsController } from './Cats/cat.controller';
 
 @Module({
   imports: [],
