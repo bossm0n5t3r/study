a8d84a6f2b6f89c53bb63cdf283f173a743a3d79
bossm0n5t3r <bossm0n5t3r@gmail.com>
2021-02-13 23:30:20 +0900

    Add item43

---
+++ demo-effective-kotlin/src/main/kotlin/com/example/demoeffectivekotlin/item43/Freq.kt
@@ -0,0 +1,27 @@
+package com.example.demoeffectivekotlin.item43
+
+import java.time.Instant
+import java.util.TreeMap
+
+fun main() {
+    val frequencyTable = TreeMap<String, Int>()
+    val args = arrayOf("a", "b", "c", "d", "e", "a")
+
+    println("Result : Lambda")
+    // Lambda
+    for (s in args) frequencyTable.merge(s, 1) { count: Int, incr: Int -> count + incr }
+    println(frequencyTable)
+    println()
+
+    // clear
+    frequencyTable.clear()
+
+    println("Result : Integer::sum")
+    // Integer::sum
+    for (s in args) frequencyTable.merge(s, 1, Integer::sum)
+    println(frequencyTable)
+    println()
+
+    // clear
+    frequencyTable.clear()
+}
