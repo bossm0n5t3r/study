0b6caafcb9194688a1ceca4c0a220009e3ad9fc2
bossm0n5t3r <bossm0n5t3r@gmail.com>
2021-03-23 19:24:44 +0900

    feature : Create ormconfig.json and apply

--- demo-nestjs/src/app.module.ts
+++ demo-nestjs/src/app.module.ts
@@ -5,18 +5,7 @@ import { AppService } from './app.service';
 import { CatsModule } from './cats/cats.module';
 
 @Module({
-  imports: [
-    CatsModule,
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: 'root',
-      database: 'test',
-      entities: [],
-    }),
-  ],
+  imports: [CatsModule, TypeOrmModule.forRoot()],
   controllers: [AppController],
   providers: [AppService],
 })
