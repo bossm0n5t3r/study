9bbf906fc1928bc2953d320cc76ae99f6e03e069
bossm0n5t3r <bossm0n5t3r@gmail.com>
2021-03-23 19:21:23 +0900

    feature : TypeORM Integration

--- demo-nestjs/src/app.module.ts
+++ demo-nestjs/src/app.module.ts
@@ -1,10 +1,22 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatsModule } from './cats/cats.module';
 
 @Module({
-  imports: [CatsModule],
+  imports: [
+    CatsModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'test',
+      entities: [],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
