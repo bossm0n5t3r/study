8802a8dd3274db96b83a06f7521285af3395d4ac
bossm0n5t3r <bossm0n5t3r@gmail.com>
2021-04-13 20:11:39 +0900

    feature : Transactional import 변경

--- demo-spring-data-jpa/src/test/kotlin/com/example/demospringdatajpa/domain/member/MemberRepositoryTest.kt
+++ demo-spring-data-jpa/src/test/kotlin/com/example/demospringdatajpa/domain/member/MemberRepositoryTest.kt
@@ -4,7 +4,7 @@ import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
-import javax.transaction.Transactional
+import org.springframework.transaction.annotation.Transactional
 
 @SpringBootTest
 @Transactional
