f4cf5008dc3c7ed01a5bcbbe99461e9e915aa733
bossm0n5t3r <bossm0n5t3r@gmail.com>
2021-05-02 00:08:20 +0900

    feature : Add isAlreadySignedUpEmail

--- demo-spring-data-jpa/src/main/kotlin/com/example/demospringdatajpa/service/EmailValidationService.kt
+++ demo-spring-data-jpa/src/main/kotlin/com/example/demospringdatajpa/service/EmailValidationService.kt
@@ -1,12 +1,22 @@
 package com.example.demospringdatajpa.service
 
+import com.example.demospringdatajpa.domain.member.MemberRepository
+import org.springframework.stereotype.Service
 import java.util.regex.Pattern
 
-class EmailValidationService {
+@Service
+class EmailValidationService(
+    private val memberRepository: MemberRepository
+) {
     private val EMAIL_PATTERN = Pattern.compile("""(?:[a-z0-9!#${'$'}%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#${'$'}%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])""")
 
     fun verifyEmail(email: String?): Boolean {
         if (email.isNullOrEmpty()) return false
         return EMAIL_PATTERN.matcher(email).matches()
+    }
+
+    fun isAlreadySignedUpEmail(email: String?): Boolean {
+        if (email.isNullOrEmpty()) return false
+        return memberRepository.findByEmail(email) != null
     }
 }
--- demo-spring-data-jpa/src/test/kotlin/com/example/demospringdatajpa/service/EmailValidationServiceTest.kt
+++ demo-spring-data-jpa/src/test/kotlin/com/example/demospringdatajpa/service/EmailValidationServiceTest.kt
@@ -1,12 +1,21 @@
 package com.example.demospringdatajpa.service
 
+import com.example.demospringdatajpa.domain.member.Member
+import com.example.demospringdatajpa.domain.member.MemberRepository
 import org.junit.jupiter.api.Assertions.assertFalse
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
 
+@SpringBootTest
 class EmailValidationServiceTest {
-    private val emailValidationService = EmailValidationService()
+    @Autowired
+    private lateinit var emailValidationService: EmailValidationService
+
+    @Autowired
+    private lateinit var memberRepository: MemberRepository
 
     @Test
     @DisplayName("이메일 형식 테스트")
@@ -38,5 +47,27 @@ class EmailValidationServiceTest {
         assertFalse(emailValidationService.verifyEmail(emailF4))
         assertFalse(emailValidationService.verifyEmail(emailF5))
         assertFalse(emailValidationService.verifyEmail(emailF6))
+    }
+
+    @Test
+    @DisplayName("이미 존재하는 이메일인지 확인합니다.")
+    fun isAlreadySignedUpEmail() {
+        // given
+        val email = "test@test.test"
+        val password = "password"
+        val name = "name"
+
+        memberRepository.save(
+            Member(
+                email = email,
+                password = password,
+                name = name
+            )
+        )
+
+        val newEmail = "newTest@test.test"
+
+        assertTrue(emailValidationService.isAlreadySignedUpEmail(email))
+        assertFalse(emailValidationService.isAlreadySignedUpEmail(newEmail))
     }
 }
